38b33a32df31db3c17ef1e57b0fad4c48af409aa
Aram Simonian <aram.contacts@gmail.com>
2017-10-02 17:06:04 +0100

    completed challenge #4

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,4 +1,11 @@
-describe Oystercard do
+require './lib/oystercard'
 
-  
+describe Oystercard do
+  subject(:oystercard) { described_class.new }
+
+  describe '#balance' do
+    it 'check a new card has a balance of zero' do
+      expect(subject.balance).to eq 0
+    end
+  end
 end
